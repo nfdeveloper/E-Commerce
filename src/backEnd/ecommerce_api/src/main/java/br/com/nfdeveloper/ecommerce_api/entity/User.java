6fe0b804dadff1104845df0d8c1ab93abab98e6e
nfdeveloper <nfdeveloper04@gmail.com>
2024-01-07 22:28:56 -0300

    Primeiro commit. Criando Api Ecommercer, configurando MysqlDB e entidade de Usuarios

---
+++ src/backEnd/ecommerce_api/src/main/java/br/com/nfdeveloper/ecommerce_api/entity/User.java
@@ -0,0 +1,113 @@
+package br.com.nfdeveloper.ecommerce_api.entity;
+
+import java.util.Objects;
+
+import br.com.nfdeveloper.ecommerce_api.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String email;
+	
+	private String password;
+	
+	private String name;
+	
+	private UserRole role;
+	
+	@Lob
+	@Column(columnDefinition = "longblob")
+	private byte[] img;
+
+	public User() {
+	
+	}
+	
+	public User(Long id, String email, String password, String name, UserRole role, byte[] img) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.role = role;
+		this.img = img;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	public byte[] getImg() {
+		return img;
+	}
+
+	public void setImg(byte[] img) {
+		this.img = img;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ src/backEnd/ecommerce_api/src/main/java/br/com/nfdeveloper/ecommerce_api/enums/UserRole.java
@@ -0,0 +1,7 @@
+package br.com.nfdeveloper.ecommerce_api.enums;
+
+public enum UserRole {
+
+	ADMIN,
+	CUSTOMER
+}
