98c4ee514df07743221b9213b5403a81ebe26a02
nfdeveloper <nfdeveloper04@gmail.com>
2024-01-07 23:40:23 -0300

    Configurando rotas iniciais - FrontEnd

--- src/frontEnd/ECommerceWeb/src/app/app.module.ts
+++ src/frontEnd/ECommerceWeb/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
