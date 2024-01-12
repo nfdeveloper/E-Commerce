953f3497f3435518366566d3f119f5084f928abf
nfdeveloper <nfdeveloper04@gmail.com>
2024-01-11 23:59:47 -0300

    Criação de Entidade de Produtos - BackEnd

---
+++ src/backEnd/ecommerce_api/src/main/java/br/com/nfdeveloper/ecommerce_api/entity/Product.java
@@ -0,0 +1,106 @@
+package br.com.nfdeveloper.ecommerce_api.entity;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private Long price;
+	
+	@Lob
+	private String description;
+	
+	@Lob
+	@Column(columnDefinition = "longblob")
+	private byte[] img;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "category_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private Category category;
+
+	public Product() {
+	
+	}
+
+	public Product(Long id, String name, Long price, String description, byte[] img, Category category) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.img = img;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public byte[] getImg() {
+		return img;
+	}
+
+	public void setImg(byte[] img) {
+		this.img = img;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	
+}
